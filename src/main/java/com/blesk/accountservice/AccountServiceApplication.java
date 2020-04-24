0bd703e5a6993ba3ff007c8a4dd7357431d50480
Bence <m.bence05@gmail.com>
2020-04-24 18:43:45 +0200

    travis ci bol pridany ako aj discoveryclient pre naming server

--- src/main/java/com/blesk/accountservice/AccountServiceApplication.java
+++ src/main/java/com/blesk/accountservice/AccountServiceApplication.java
@@ -2,8 +2,10 @@ package com.blesk.accountservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
+@EnableDiscoveryClient
 @EnableResourceServer
 @SpringBootApplication
 public class AccountServiceApplication {
