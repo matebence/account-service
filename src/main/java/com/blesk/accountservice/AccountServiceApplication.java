546c9acd2200641af59edeacc348f4f008d68bd5
Bence <m.bence05@gmail.com>
2020-04-25 16:28:17 +0200

    premiestnenie enable anotation to main suboru

--- src/main/java/com/blesk/accountservice/AccountServiceApplication.java
+++ src/main/java/com/blesk/accountservice/AccountServiceApplication.java
@@ -3,11 +3,15 @@ package com.blesk.accountservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @EnableDiscoveryClient
 @EnableResourceServer
 @SpringBootApplication
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class AccountServiceApplication {
 
     public static void main(String[] args) {
