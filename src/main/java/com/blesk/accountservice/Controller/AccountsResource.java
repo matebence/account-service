4d6960595b6cdcf20ff400bc601ebb30ea2cd969
Bence <m.bence05@gmail.com>
2020-06-14 19:49:58 +0200

    zmena join funkcie

--- src/main/java/com/blesk/accountservice/Controller/AccountsResource.java
+++ src/main/java/com/blesk/accountservice/Controller/AccountsResource.java
@@ -112,7 +112,7 @@ public class AccountsResource {
         return collectionModel;
     }
 
-    @PreAuthorize("hasRole('ADMIN') || hasRole('MANAGER') || hasRole('CLIENT') || hasRole('COURIER')")
+    @PreAuthorize("hasRole('SYSTEM') || hasRole('ADMIN') || hasRole('MANAGER') || hasRole('CLIENT') || hasRole('COURIER')")
     @PostMapping("/accounts/join/{columName}")
     @ResponseStatus(HttpStatus.OK)
     public CollectionModel<List<Accounts>> joinAccounts(@PathVariable String columName, @RequestBody List<Long> ids, HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
