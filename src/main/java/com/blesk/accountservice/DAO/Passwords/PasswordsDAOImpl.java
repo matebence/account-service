e4b21b334d1f44b7b9e5d53175390f17003a56e6
Bence <m.bence05@gmail.com>
2020-06-20 12:09:15 +0200

    odstranenei softdelete z passworddaoimpl

--- src/main/java/com/blesk/accountservice/DAO/Passwords/PasswordsDAOImpl.java
+++ src/main/java/com/blesk/accountservice/DAO/Passwords/PasswordsDAOImpl.java
@@ -2,23 +2,8 @@ package com.blesk.accountservice.DAO.Passwords;
 
 import com.blesk.accountservice.DAO.DAOImpl;
 import com.blesk.accountservice.Model.Passwords;
-import org.hibernate.Session;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class PasswordsDAOImpl extends DAOImpl<Passwords> implements PasswordsDAO {
-
-    @Override
-    public Boolean softDelete(Passwords passwords) {
-        Session session = this.entityManager.unwrap(Session.class);
-        try {
-            passwords = this.entityManager.contains(passwords) ? passwords : this.entityManager.merge(passwords);
-            session.delete(passwords);
-        } catch (Exception e) {
-            session.clear();
-            session.close();
-            return false;
-        }
-        return true;
-    }
 }
